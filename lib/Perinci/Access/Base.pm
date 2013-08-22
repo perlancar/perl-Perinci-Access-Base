6bb6c92ea83756e8e3e81a74b992cbc75579793e
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-08-22 13:16:36 +0700

    Remove calling _init() to simplify stuffs, add POD

--- lib/Perinci/Access/Base.pm
+++ lib/Perinci/Access/Base.pm
@@ -8,9 +8,7 @@ use warnings;
 
 sub new {
     my ($class, %opts) = @_;
-    my $obj = bless \%opts, $class;
-    $obj->_init();
-    $obj;
+    bless \%opts, $class;
 }
 
 our $re_var     = qr/\A[A-Za-z_][A-Za-z_0-9]*\z/;
@@ -46,4 +44,30 @@ sub check_request {
 }
 
 1;
-# ABSTRACT: Base class for Perinci Riap clients
+# ABSTRACT: Base class for all Perinci Riap clients
+
+=head1 DESCRIPTION
+
+This is a thin base class for all Riap clients (C<Perinci::Access::*>). It
+currently only provides check_request() to do some basic sanity checking of the
+Riap request hash C<$req>. It also provides a barebones C<new()>.
+
+=head1 METHODS
+
+=head2 new(%args) => OBJ
+
+Constructor. Does nothing except creating a blessed hashref from C<%args>.
+Subclasses should override this method and do additional stuffs as needed.
+
+=head2 check_request($req) => RESP|undef
+
+Should be called by subclasses during the early phase in C<request()>. Will
+return an enveloped error response on error, or undef on success.
+
+
+=head1 SEE ALSO
+
+L<Perinci::Access>
+
+=cut
+
